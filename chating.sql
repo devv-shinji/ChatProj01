80f3cf7a2d205476a2ca5ea378f20b600e6a7e58
devv.shinji <devv.shinji@gmail.com>
2020-05-04 09:35:31 +0900

    05-04

--- chating.sql
+++ chating.sql
@@ -13,6 +13,31 @@ create table chating_tb (
     time varchar2(100) default sysdate,
     primary key(idx)
 );
+-----------------------------네트워크 프로그래밍 평가-----------------------------
+--테이블 생성
+create table chatting_tb (
+    idx number,
+    client_name varchar2(500),
+    client_message varchar2(500),
+    senddate date
+);
+--코멘트 추가
+comment on column chatting_tb.idx is '일련번호';
+comment on column chatting_tb.client_name is '이름';
+comment on column chatting_tb.client_message is '메세지';
+comment on column chatting_tb.senddate is '전송날짜와시간';
+
+--시퀀스 생성
+create sequence chatting_seq
+    increment by 1   /* 증가치 */
+    start with 1     /* 시작값 */
+    nomaxvalue       /* 최대값 */
+    minvalue 1       /* 최소값 */
+    nocycle          /* 사이클 사용여부 */
+    nocache;         /* 캐쉬 사용여부 */
+-----------------------------네트워크 프로그래밍 평가-----------------------------
+
+
 
 --테이블 삭제
 drop table chating_tb;
